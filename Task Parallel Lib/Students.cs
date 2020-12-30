346085ff007623316e116656485474963463262b
sowmyatellapragada <63146223+sowmyatellapragada@users.noreply.github.com>
2020-12-30 18:54:52 +0530

    Add files via upload

---
+++ Task Parallel Lib/Students.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{ 
+
+
+    public class Students
+    {
+        public int Id { get; set; }
+
+        public string StudentName { get; set; }
+
+
+        public int StudentFee { get; set; }
+
+        public int Allowance { get; set; }
+
+
+        public int TotalAllowance { get; private set; }
+
+
+        public void CalculateTotalFeeToCollege()
+        {
+            this.TotalAllowance = this.StudentFee - this.Allowance;
+            Thread.Sleep(this.TotalAllowance);
+        }
+    }
+}
